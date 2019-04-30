b9190cf87276e32a2fb7514129fec631c7082b59
Reese Lee and Stuart McKay <stuart@>
2019-04-30 10:11:53 -0700

    add all files and folders for this project according to the lessons

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+      public ActionResult Index()
+      {
+        return new EmptyResult();
+      }
+
+    }
+}
