c48342ccc3accd8c863d41201ac6a5a0cdf13fb6
Reese Lee and Stuart McKay <stuart@>
2019-04-30 16:35:53 -0700

    add code to assign items IDs, and to hyperlink IDs so you can click on them.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -14,7 +14,7 @@ namespace ToDoList.Controllers
     }
 
     [HttpGet("/items/new")]
-    public ActionResult CreateForm()
+    public ActionResult New()
     {
       return View();
     }
@@ -32,5 +32,13 @@ namespace ToDoList.Controllers
       Item.ClearAll();
       return View();
     }
+
+    [HttpGet("/items/{id}")]
+     public ActionResult Show(int id)
+     {
+       Item item = Item.Find(id);
+       return View(item);
+     }
+
   }
 }
